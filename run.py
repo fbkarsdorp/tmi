551fef31d78745d1afe06499f5a1feb15e0480dd
BSTN <stokhuyzen@gmail.com>
2013-10-27 19:02:48 +0100

    update with files...

---
+++ run.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import os
+from flask import Flask, request, jsonify
+from TMI import TMI
+import cPickle as pickle
+
+#flask application
+app = Flask(__name__)
+
+# views:
+@app.route('/api',methods=['GET', 'POST'])
+def api():
+
+	query = request.form['q']
+	
+	with open("tmi.cPickle") as inf:
+		tmi = pickle.load(inf)
+
+	results = ""
+	for match in tmi.search(query):
+		idee = match[0]
+		text = match[1]
+		results += "<div id='match'><span id='idee'>"+idee+"</span><span id='text'>"+text+"</span></div>"
+	return jsonify({"html":results})
+
+@app.route('/')
+def index():
+	return open(os.getcwd()+'/static/templates/index.html').read()
+
+if __name__ == '__main__':
+	app.run(debug=True,host='localhost',port=5555,use_reloader=True,threaded=False)
+
+
+
+
---
+++ static/js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+	$("#search input").on("keydown",function(e){
+		if(e.keyCode == "13"){
+			$("#results").html("");
+			$("#results").addClass("loading");
+			query = $(this).val();
+			console.log(query);
+			$.ajax({
+				url:"/api",
+				data:{"q":query},
+				method:"POST",
+				success:function(data){
+					console.log(data);
+					$("#results").removeClass("loading");
+					$("#results").html(data.html);
+				},
+				error:function(){
+					$("#results").removeClass("loading");
+					$("#results").html("oops, something went wrong...");
+				}
+			})
+		}
+	})
+
+})
