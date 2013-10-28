dcb4fbc46e9599b49f6472b86a8e90924801ee64
Folgert Karsdorp <fbkarsdorp@gmail.com>
2013-10-28 13:38:28 +0100

    updates

--- run.py
+++ run.py
@@ -10,33 +10,33 @@ import cPickle as pickle
 app = Flask(__name__)
 
 with open("tmi.cPickle") as inf:
-	tmi = []
-	while True:
-		try:
-			motif = pickle.load(inf)
-			tmi.append(motif)
-		except EOFError:
-			break
+    tmi = []
+    while True:
+        try:
+            motif = pickle.load(inf)
+            tmi.append(motif)
+        except EOFError:
+            break
 
 # views:
 @app.route('/api',methods=['GET', 'POST'])
 def api():
 
-	query = request.form['q'].strip()
+    query = request.form['q'].strip()
 
-	results = ""
-	for match in find(query, tmi):
-		idee = match[0]
-		text = match[1]
-		results += "<div id='match'><span id='idee'>"+idee+"</span><span id='text'>"+text+"</span></div>"
-	return jsonify({"html":results})
+    results = ""
+    for match in find(query, tmi):
+        idee = match[0]
+        text = match[1]
+        results += "<div id='match'><span id='idee'>"+idee+"</span><span id='text'>"+text+"</span></div>"
+    return jsonify({"html":results})
 
 @app.route('/')
 def index():
-	return open(os.getcwd()+'/static/templates/index.html').read()
+    return open(os.getcwd()+'/static/templates/index.html').read()
 
 if __name__ == '__main__':
-	app.run(debug=True,host='localhost',port=5555,use_reloader=False,threaded=False)
+    app.run(debug=True,host='localhost',port=5555,use_reloader=False,threaded=False)
 
 
 
---
+++ tmi.py
@@ -0,0 +1,40 @@
+import cPickle
+
+from pattern.en import Sentence, wordnet
+from pattern.search import taxonomy, search, Classifier
+
+
+# /////////////////////////////////////////////////////////////////////////////
+# Wordnet Classifier used for searching.
+# /////////////////////////////////////////////////////////////////////////////
+
+class WordNetClassifier(Classifier):
+    
+    def __init__(self, wordnet=None):
+        if wordnet is None:
+            try: 
+                from en import wordnet
+            except ImportError:
+                pass
+        Classifier.__init__(self, self._parents, self._children)
+        self.wordnet = wordnet
+
+    def _children(self, word, pos="NN"):
+        try: 
+            return [w.senses[0] for w in self.wordnet.synsets(word, pos)[0].hyponyms()]
+        except (KeyError, IndexError):
+            pass
+        
+    def _parents(self, word, pos="NN"):
+        try: 
+            return [w.senses[0] for w in self.wordnet.synsets(word, pos)[0].hypernyms()]
+        except (KeyError, IndexError):
+            pass
+
+taxonomy.classifiers.append(WordNetClassifier(wordnet))
+
+def find(pattern, motifs):
+    for motif, (description, parse) in motifs:
+        if search(pattern, parse):
+            yield motif, description
+        
